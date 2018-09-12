a92d7cd8a5c4f7f57e13608e445bb731837a0b8a
pperezp <patodeath@gmail.com>
2018-09-12 11:48:08 -0300

    Puse 4 lineas de código

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,3 +1,8 @@
 print ("josele")
 print ("Cualquier cuestion porque el profe dijo")
 print("Profe Pato")
+
+# mis cambios --> falta de comunicación
+print("print1")
+print("print2")
+print("print3")
